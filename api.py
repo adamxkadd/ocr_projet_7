d4f5617e087324ea0bd19cf6c01af0a883f9bdb0
adamxkadd <143011477+adamxkadd@users.noreply.github.com>
2023-09-07 14:46:23 +0200

    Update api.py

--- api.py
+++ api.py
@@ -4,15 +4,16 @@ import pickle
 import pandas as pd
 from flask import Flask, request, jsonify
 
-# Init Flask app
+# Flask app initialisation
 app = Flask(__name__)
 
-# Load model objects
-model_path = os.path.join(os.path.dirname(__file__), "model.pkl")
-with open(model_path, "rb") as handle:
-    transformer, classifier = pickle.load(handle)
 
-    
+# charger le modele
+file = open("model.pkl", 'rb')
+transformer, classifier = pickle.load(file)
+file.close()
+
+
 @app.route("/predict", methods=["POST","GET"])
 def predict():
     try:
@@ -30,24 +31,6 @@ def predict():
 
     except Exception as e:
         return jsonify(error=str(e)), 500  
-
-    
-    
-    
-    
-
-
-# @app.route("/predict", methods=["POST","GET"])
-# def predict():
-#     # Parse data as JSON
-#     client_input = request.get_json()
-#     client_input = pd.DataFrame(client_input)
-#     # client_input = transformer.transform(client_input)
-#     # pred = classifier.predict(client_input)[0]
-#     # proba = classifier.predict_proba(client_input)[0][pred]
-#     pred = 0
-#     proba = 0.275      
-#     return jsonify(prediction=int(pred), probability=round(100 * proba, 1))
 
 
 if __name__ == '__main__':
