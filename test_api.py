6e0d69b6bffef207c9317b90e23e613ce32d9535
adamxkadd <143011477+adamxkadd@users.noreply.github.com>
2023-09-10 16:40:57 +0200

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -7,10 +7,10 @@ from api import app
 @pytest.fixture
 def client():
     return app.test_client()
-    
+
 def test_predict(client):
-    # Créez des données de client en df 
-    client = [
+    # Créez des données de client factices au format JSON
+    client_data = [
         {
             "SK_ID_CURR": 100875,
             "NAME_CONTRACT_TYPE": "Cash loans",
@@ -20,29 +20,54 @@ def test_predict(client):
         }
     ]
     
-    client_df = pd.DataFrame(client)
-    client_json = json.loads(client_df.to_json())
-    print("client_json : ", client_json)
-    # Convertissez les données en JSON 
-    # client_data_json = json.dumps(client_data)
-      
     # Envoyez une requête POST à la route /predict
-    URL = "https://scoring-credit.streamlit.app/predict"
-    response = requests.post(URL, json=client_json, timeout=120)
-    print("response : ", response.text)
-    
-    data = response #.json()
-    pred, proba = data["prediction"], data["probability"]
-    print("Probabilité du risque : {}%".format(proba))
-    # response = requests.post(URL, json=client_data, timeout=120)
-    # data = response.json()
+    response = client.post("/predict", json=client_data)
     
     # Vérifiez le code de réponse HTTP
     assert response.status_code == 200
 
+    data = response.get_json()
     
-    # # Vérifiez les valeurs renvoyées
-    # assert "prediction" in data
-    # assert "probability" in data
-    # assert isinstance(data["prediction"], int)
-    # assert isinstance(data["probability"], float)
+    
+# def test_predict(client):
+#     # Créez des données de client en df 
+#     client = [
+#         {
+#             "SK_ID_CURR": 100875,
+#             "NAME_CONTRACT_TYPE": "Cash loans",
+#             "AMT_INCOME_TOTAL": 20002,
+#             "AMT_CREDIT": 20000,
+#             "AMT_ANNUITY": 2000,
+#         }
+#     ]
+    
+#     client_df = pd.DataFrame(client)
+#     client_json = json.loads(client_df.to_json())
+#     print("client_json : ", client_json)
+#     # Convertissez les données en JSON 
+#     # client_data_json = json.dumps(client_data)
+      
+#     # Envoyez une requête POST à la route /predict
+#     URL = "https://scoring-credit.streamlit.app/predict"
+#     response = requests.post(URL, json=client_json, timeout=120)
+#     print("response : ", response.text)
+
+#     data = json.loads(response_text)
+#     pred, proba = data["prediction"], data["probability"]
+#     print("Probabilité du risque : {}%".format(proba))
+
+#     # data = response #.json()
+#     # pred, proba = data["prediction"], data["probability"]
+#     # print("Probabilité du risque : {}%".format(proba))
+#     # response = requests.post(URL, json=client_data, timeout=120)
+#     # data = response.json()
+    
+#     # Vérifiez le code de réponse HTTP
+#     assert response.status_code == 200
+
+    
+#     # # Vérifiez les valeurs renvoyées
+#     # assert pred in data
+#     # assert proba in data
+#     # assert isinstance(data["prediction"], int)
+#     # assert isinstance(data["probability"], float)
